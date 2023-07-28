37ce6c9dd0074a5f348bcf184113af6a8acaabc8
aryansetia <aryansetiaa@gmail.com>
2023-07-28 09:35:03 +0530

    best to buy and sell stock

---
+++ Arrays/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,15 @@
+"""
+121. Best Time to Buy and Sell Stock
+
+Problem link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = 10 ** 8 
+        profit = 0 
+        for i in range(len(prices)):
+            min_price = min(min_price, prices[i])
+            profit = max(profit, prices[i]-min_price)
+
+        return profit 
